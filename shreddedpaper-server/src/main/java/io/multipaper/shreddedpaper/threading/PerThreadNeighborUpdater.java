3d653badaef7d40e9224708b6ede88118de87f01
PureGero <puregero@gmail.com>
2026-01-02 19:04:46 +0900

    Thread-safe NeigbhorUpdater

--- shreddedpaper-server/src/main/java/io/multipaper/shreddedpaper/threading/PerThreadNeighborUpdater.java
+++ shreddedpaper-server/src/main/java/io/multipaper/shreddedpaper/threading/PerThreadNeighborUpdater.java
@@ -5,8 +5,8 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.redstone.NeighborUpdater;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.world.level.redstone.Orientation;
+import org.jspecify.annotations.Nullable;
 
 import java.util.function.Supplier;
 
@@ -23,22 +23,22 @@ public class PerThreadNeighborUpdater implements NeighborUpdater {
     }
 
     @Override
-    public void shapeUpdate(@NotNull Direction direction, @NotNull BlockState neighborState, @NotNull BlockPos pos, @NotNull BlockPos neighborPos, int flags, int maxUpdateDepth) {
-        getOrCreate().shapeUpdate(direction, neighborState, pos, neighborPos, flags, maxUpdateDepth);
+    public void shapeUpdate(Direction direction, BlockState state, BlockPos pos, BlockPos neighborPos, @Block.UpdateFlags int flags, int recursionLeft) {
+        getOrCreate().shapeUpdate(direction, state, pos, neighborPos, flags, recursionLeft);
     }
 
     @Override
-    public void neighborChanged(@NotNull BlockPos pos, @NotNull Block sourceBlock, @NotNull BlockPos sourcePos) {
-        getOrCreate().neighborChanged(pos, sourceBlock, sourcePos);
+    public void neighborChanged(BlockPos pos, Block neighborBlock, @Nullable Orientation orientation) {
+        getOrCreate().neighborChanged(pos, neighborBlock, orientation);
     }
 
     @Override
-    public void neighborChanged(@NotNull BlockState state, @NotNull BlockPos pos, @NotNull Block sourceBlock, @NotNull BlockPos sourcePos, boolean notify) {
-        getOrCreate().neighborChanged(state, pos, sourceBlock, sourcePos, notify);
+    public void neighborChanged(BlockState state, BlockPos pos, Block neighborBlock, @Nullable Orientation orientation, boolean movedByPiston) {
+        getOrCreate().neighborChanged(state, pos, neighborBlock, orientation, movedByPiston);
     }
 
     @Override
-    public void updateNeighborsAtExceptFromFacing(@NotNull BlockPos pos, @NotNull Block sourceBlock, @Nullable Direction except) {
-        getOrCreate().updateNeighborsAtExceptFromFacing(pos, sourceBlock, except);
+    public void updateNeighborsAtExceptFromFacing(BlockPos pos, Block block, @Nullable Direction facing, @Nullable Orientation orientation) {
+        getOrCreate().updateNeighborsAtExceptFromFacing(pos, block, facing, orientation);
     }
 }
