251317fd45b1d4a8d632f3bfaac2c64784958662
PureGero <puregero@gmail.com>
2026-09-25 17:25:44 +0200

    26.3 progress

--- purpurRefLatest.sh
+++ purpurRefLatest.sh
@@ -7,7 +7,7 @@ setProperty(){
   mv $3.tmp $3
 }
 
-SHA=$(curl https://api.github.com/repos/PurpurMC/Purpur/commits/ver/26.2 | jq -r .sha)
+SHA=$(curl https://api.github.com/repos/PurpurMC/Purpur/commits/ver/26.3 | jq -r .sha)
 setProperty purpurRef $SHA gradle.properties
 
 echo $SHA
